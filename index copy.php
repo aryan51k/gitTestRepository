9f6853d82fa6dd10ddd915366c06e8bde043839c
aryan51k <75057473+aryan51k@users.noreply.github.com>
2020-11-26 11:24:29 +0530

    made changes

---
+++ index copy.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+
+       
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+    </head>
+
+    <body>
+        <p>This is a basic app</p>
+        <style>
+            .form_look{
+                width: 700px;
+
+            }
+        </style>
+        <div ng-app ="login_register" ng-controller = "myControl" class = "form_look">
+
+    </body>
+
+</html>
--- index.php
+++ index.php
@@ -1,1 +1,2 @@
 <h1>Hello PHP world </h1>
+<h2> This is the first time I am using GIT HUB it is intresting </h2>
